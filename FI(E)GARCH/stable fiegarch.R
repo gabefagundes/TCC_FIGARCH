56db087a390a9911bf47687755c5ad3fa42c1dce
Gabriel Fagundes <73248307+gabefagundes@users.noreply.github.com>
2022-02-04 09:18:48 -0300

    ultima versao do stable_fiegarch

--- FI(E)GARCH/stable fiegarch.R
+++ FI(E)GARCH/stable fiegarch.R
@@ -7,6 +7,8 @@
 {
     library(stabledist)
     library(tidyverse)
+    medias_aux = read_csv2('FI(E)GARCH/arquivos_auxiliares/simulacoes_abs_alpha_estavel_medias.csv') %>% 
+        select(alpha, media)
 }
 
 
@@ -25,19 +27,24 @@ pi_func = function(k, d){
     return(out)
 }
 
-g_func = function(z, 
+
+E_X = medias_aux %>% 
+    filter(alpha == 1.8) %>% 
+    select(media) %>% 
+    pull()
+
+g_func_stable = function(z, 
                   theta, 
                   gamma,
-                  params_stable = c('alpha' = 2, 'gamma' = 1)){
+                  alpha_stable = 2){
     
     # função feita para v.a.'s normais 
-    
     
     if(z < 0){
         gamma = -gamma
     }
     
-    out = (theta + gamma)*z - gamma*1.37
+    out = (theta + gamma)*z - gamma*E_X
 }
 
 
@@ -68,7 +75,7 @@ poly.prod <- function(a = 1,b = 1){
 }
 
 
-lambda = function(alpha, beta, trunc, d){
+lambda = function(trunc, d, alpha, beta){
     
     p = c(1, alpha)
     q = polyinv(c(1, beta), m = trunc)
@@ -77,6 +84,8 @@ lambda = function(alpha, beta, trunc, d){
     s = sapply(0:trunc, pi_func, d)
     
     out = poly.prod(pq,s)[1:(trunc+1)]
+    
+    return(out)
     
 }
 
@@ -104,7 +113,7 @@ lambda2 = function(trunc, d, alpha, beta){
             #print(paste0("i= ", i))
             parcelas_soma2 = numeric(length(1:(k - i + 1)))
             for (j in 1:(k - i + 1)){
-                print(paste0("j= ", j))
+                #print(paste0("j= ", j))
                 parcelas_soma2[j] = beta[j]*pi_func(k-i-j+1, -d)
                 #print(paste0('pi_func= ', pi_func(k-i-j+1, -d), ', beta_j = ', beta[j]))
             }
@@ -144,19 +153,21 @@ fiegarch_1d1 = function(N = 500,
                 delta = 0
                 ) 
     x = numeric(N + trunc)
+    print("Gerou variaveis iniciais, Começando a gerar valores da série.")
+    
     
     for (t in (trunc+1):(N+trunc)){
-        
+        print(t)
         aux = (t):(t-trunc)
         
-        sigma[t] = (a0 + sum( lambda(alpha, beta, trunc, d)*sapply(z[aux], g_func, theta = theta, gamma = gamma))) %>% 
+        sigma[t] = (a0 + sum(lambda(trunc, d, alpha, beta)*sapply(z[aux], g_func_stable, theta = theta, gamma = gamma, alpha_stable = alpha_stable))) %>% 
             exp() %>% 
             sqrt()
         
         x[t] = sigma[t]*z[t + 1]
         
     }
-    
+    print("Series e Sigmas Gerados")
     y = x[(trunc+1):(trunc + N)]
     sigma = sigma[(trunc+1):(trunc + N)]
     
@@ -171,14 +182,15 @@ fiegarch_1d1 = function(N = 500,
 
 
 {
-    N = 2000
-    d = 0.9
+    N = 1000
+    d = 0.7
     a0 = -9
     alpha = 0.1
     beta = 0.68
     theta = -0.21661
     gamma = 0.27
     trunc = 100
+    alpha_stable = 1.8
 }
 
 teste = fiegarch_1d1(N, d, a0, alpha, beta, theta, gamma, trunc)
@@ -190,5 +202,4 @@ teste = fiegarch_1d1(N, d, a0, alpha, beta, theta, gamma, trunc)
 }
 
 
-Box.test(teste$serie, lag = 20, type = 'Ljung-Box')
-
+Box.test(teste$serie, lag = 20, type = 'Box-Pierce')
